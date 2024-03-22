3543d659ef80df50306c858fa8562db13d40d2a3
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-22 15:45:22 +0600

    active link props type

--- src/components/ui/ActiveLink.tsx
+++ src/components/ui/ActiveLink.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
 
-const ActiveLink = ({ href, exact = false, children }) => {
+type TActiveLinkProps = {
+  href: string;
+  exact?: boolean;
+  children: ReactNode;
+};
+
+const ActiveLink = ({ href, exact = false, children }: TActiveLinkProps) => {
   const path = usePathname();
   const active = exact ? path == href : path.startsWith(href);
 
