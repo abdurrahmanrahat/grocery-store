fd30459a59b398e5dc93780991d1d67227faf10c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-03-23 15:44:30 +0600

    flash sale card

--- src/components/HomePage/FlashSale/FlashSale.tsx
+++ src/components/HomePage/FlashSale/FlashSale.tsx
@@ -1,5 +1,16 @@
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
-import { Box, Button, Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 
 const FlashSale = async () => {
@@ -29,7 +40,75 @@ const FlashSale = async () => {
         </div>
 
         {/* card */}
-        <div></div>
+        <div className="mt-8">
+          <Grid container spacing={2}>
+            {fishes?.slice(0, 4).map((fish) => (
+              <Grid item key={fish._id} md={3}>
+                <Card
+                  sx={
+                    {
+                      // border: "none",
+                    }
+                  }
+                >
+                  <Box
+                    sx={{
+                      position: "relative",
+                    }}
+                  >
+                    <Image
+                      src={fish.image[0]}
+                      width={500}
+                      height={100}
+                      alt="Image"
+                    />
+                    <Box
+                      sx={{
+                        position: "absolute",
+                        top: "10px",
+                        left: "10px",
+                        backgroundColor: "primary.dark",
+                        padding: "8px",
+                        color: "primary.light",
+                        fontSize: "12px",
+                        borderRadius: "10px",
+                      }}
+                    >
+                      -{fish.discountPercentage}%
+                    </Box>
+                  </Box>
+                  <CardContent>
+                    <Typography variant="h6" component="div" fontWeight={600}>
+                      {fish.title}
+                    </Typography>
+                  </CardContent>
+                  <CardActions
+                    sx={{
+                      justifyContent: "space-between",
+                      px: 2,
+                      paddingBottom: "20px",
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{
+                        display: "flex",
+                        gap: "8px",
+                      }}
+                    >
+                      <Box fontWeight={600}>${fish.price}</Box>{" "}
+                      <Box color="primary.dark" fontWeight={600}>
+                        ${fish.price - 20}
+                      </Box>
+                    </Typography>
+                    <AddCircleOutlineIcon />
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </Container>
     </div>
   );
