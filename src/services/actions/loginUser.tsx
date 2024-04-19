f9f5286aee90b989cffef20433aff48d77a10bf7
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-19 22:36:40 +0600

    login function

---
+++ src/services/actions/loginUser.tsx
@@ -0,0 +1,17 @@
+"use server";
+
+import { TLoginUser } from "@/types";
+
+export const registerUser = async (userData: TLoginUser) => {
+  const res = await fetch(`${process.env.BACKED_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+    cache: "no-store",
+  });
+  const userInfo = await res.json();
+
+  return userInfo;
+};
--- src/types/user.ts
+++ src/types/user.ts
@@ -3,3 +3,8 @@ export type TRegisterUser = {
   email: string;
   password: string;
 };
+
+export type TLoginUser = {
+  email: string;
+  password: string;
+};
