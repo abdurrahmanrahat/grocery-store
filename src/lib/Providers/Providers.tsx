de2845620a3f331d047b24e63dff50e2fee9a0fd
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-20 12:08:32 +0600

    redux initialize

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { store } from "@/redux/store";
 import { ThemeProvider } from "@emotion/react";
 import { ReactNode } from "react";
+import { Provider } from "react-redux";
 import { theme } from "../theme/theme";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
+  );
 };
 
 export default Providers;
