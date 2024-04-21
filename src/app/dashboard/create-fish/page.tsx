c82b5acfc73298ab4e9227ebafe19152f12ee522
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-21 09:37:18 +0600

    image host in image bb

--- src/app/dashboard/create-fish/page.tsx
+++ src/app/dashboard/create-fish/page.tsx
@@ -1,12 +1,36 @@
 "use client";
 
+import GSFileUploader from "@/components/Forms/GSFileUploader";
 import GSForm from "@/components/Forms/GSForm";
 import GSInput from "@/components/Forms/GSInput";
-import { Container, Grid, Stack } from "@mui/material";
+import { Button, Container, Grid, Stack } from "@mui/material";
+
+const img_hosting_token = "a272d7fb3d5b5ee711a07f62d1b2c93f";
+// console.log(img_hosting_token);
 
 const CreateFish = () => {
+  const img_hosting_url = `https://api.imgbb.com/1/upload?key=${img_hosting_token}`;
+
   const handleCreateFish = (values: any) => {
     console.log(values);
+
+    // img hosting to imgbb
+    const formData = new FormData();
+    formData.append("image", values.image);
+
+    fetch(img_hosting_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then(async (imgRes) => {
+        if (imgRes.success) {
+          const image = imgRes.data.display_url;
+          console.log(image);
+
+          // Send new supply to database store
+        }
+      });
   };
 
   return (
@@ -28,7 +52,22 @@ const CreateFish = () => {
             <Grid item md={6}>
               <GSInput name="title" label="Fish Title" />
             </Grid>
+            <Grid item md={6}>
+              <GSFileUploader name="image" label="Upload File" />
+            </Grid>
           </Grid>
+
+          {/* submit button */}
+          <Button
+            sx={{
+              marginY: "16px",
+              fontSize: "17px",
+            }}
+            fullWidth={true}
+            type="submit"
+          >
+            Create
+          </Button>
         </GSForm>
       </Stack>
     </Container>
---
+++ src/components/Forms/GSFileUploader.tsx
@@ -0,0 +1,44 @@
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { Input } from "@mui/material";
+import Button from "@mui/material/Button";
+import { SxProps } from "@mui/material/styles";
+import { Controller, useFormContext } from "react-hook-form";
+
+type TFileProps = {
+  name: string;
+  label?: string;
+  sx?: SxProps;
+};
+
+export default function GSFileUploader({ name, label, sx }: TFileProps) {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { onChange, value, ...field } }) => {
+        return (
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            startIcon={<CloudUploadIcon />}
+            sx={{ ...sx }}
+          >
+            {label || "Upload File"}
+            <Input
+              {...field}
+              type="file"
+              value={value?.fileName}
+              onChange={(e) =>
+                onChange((e?.target as HTMLInputElement).files?.[0])
+              }
+              style={{ display: "none" }}
+            />
+          </Button>
+        );
+      }}
+    />
+  );
+}
