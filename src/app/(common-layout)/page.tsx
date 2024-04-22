1210c0b6271603d2465b4ce0880b758282a3025b
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-22 22:00:47 +0600

    features section design

--- src/app/(common-layout)/page.tsx
+++ src/app/(common-layout)/page.tsx
@@ -1,4 +1,5 @@
 import BannerCarousel from "@/components/HomePage/BannerCarousel/BannerCarousel";
+import Features from "@/components/HomePage/Features/Features";
 import FlashSale from "@/components/HomePage/FlashSale/FlashSale";
 import PopularProducts from "@/components/HomePage/PopularProducts/PopularProducts";
 import TopCategory from "@/components/HomePage/TopCategory/TopCategory";
@@ -7,6 +8,7 @@ const HomePage = () => {
   return (
     <div>
       <BannerCarousel />
+      <Features />
       <FlashSale />
       <TopCategory />
       <PopularProducts />
--- src/components/HomePage/Features/Features.tsx
+++ src/components/HomePage/Features/Features.tsx
@@ -2,6 +2,8 @@ import FeatureImg1 from "@/asserts/home/features/1.svg";
 import FeatureImg2 from "@/asserts/home/features/2.svg";
 import FeatureImg3 from "@/asserts/home/features/3.svg";
 import FeatureImg4 from "@/asserts/home/features/4.svg";
+import { Box, Container } from "@mui/material";
+import Image from "next/image";
 
 const featuresData = [
   {
@@ -31,7 +33,42 @@ const featuresData = [
 ];
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <Container
+      sx={{
+        marginY: "60px",
+      }}
+    >
+      <div className="grid grid-cols-12 gap-[30px] lg:gap-[40px] 2xl:gap-[50px]">
+        {featuresData?.map((item) => (
+          <div
+            key={item.id}
+            className="col-span-12 md:col-span-6 lg:col-span-3 border-b-[3px] border-solid border-[#0095CF]"
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "10px",
+                boxShadow: 1,
+                paddingX: "20px",
+                paddingY: "40px",
+                borderRadius: "0px 80px 0px 0px",
+              }}
+            >
+              <Image src={item.image} width={80} height={80} alt={item.title} />
+              <h2 className="text-[20px] font-bold">{item.title}</h2>
+              <p className="text-center text-[#929AAB] leading-7">
+                {item.desc}
+              </p>
+            </Box>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 export default Features;
