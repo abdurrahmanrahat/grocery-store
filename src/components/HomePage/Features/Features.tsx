1c9787205340ec49e03dbf142740ef03fe59f991
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-22 20:56:26 +0600

    features section data

---
+++ src/components/HomePage/Features/Features.tsx
@@ -0,0 +1,37 @@
+import FeatureImg1 from "@/asserts/home/features/1.svg";
+import FeatureImg2 from "@/asserts/home/features/2.svg";
+import FeatureImg3 from "@/asserts/home/features/3.svg";
+import FeatureImg4 from "@/asserts/home/features/4.svg";
+
+const featuresData = [
+  {
+    id: 1,
+    image: FeatureImg1,
+    title: "Expert Staff",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of the page.",
+  },
+  {
+    id: 2,
+    image: FeatureImg2,
+    title: "Tips & Advice",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of the page.",
+  },
+  {
+    id: 3,
+    image: FeatureImg3,
+    title: "Fishing Spot",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of the page.",
+  },
+  {
+    id: 4,
+    image: FeatureImg4,
+    title: "Quick Membership",
+    desc: "It is a long established fact that a reader will be distracted by the readable content of the page.",
+  },
+];
+
+const Features = () => {
+  return <div>Features</div>;
+};
+
+export default Features;
