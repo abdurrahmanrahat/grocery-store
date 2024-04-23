55d94b9b15ca497d397b83e21b6b6dd71c5510b8
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-04-23 11:18:10 +0600

    articles data

---
+++ src/components/HomePage/ArticlesSection/ArticlesSection.tsx
@@ -0,0 +1,45 @@
+import ArticleImg1 from "@/asserts/home/articles/1.jpg";
+import ArticleImg2 from "@/asserts/home/articles/2.jpg";
+import ArticleImg3 from "@/asserts/home/articles/3.jpg";
+import ArticleImg4 from "@/asserts/home/articles/4.jpg";
+import ArticleIcon1 from "@/asserts/home/articles/icon1.svg";
+import ArticleIcon2 from "@/asserts/home/articles/icon2.svg";
+import ArticleIcon3 from "@/asserts/home/articles/icon3.svg";
+import ArticleIcon4 from "@/asserts/home/articles/icon4.svg";
+
+const articleBlogs = [
+  {
+    id: 1,
+    title: "Solo & Team Fishing",
+    image: ArticleImg1,
+    icon: ArticleIcon1,
+    desc: "There are many variations of passages orem psum available but the majority have suffered alteration in some form by injected.",
+  },
+  {
+    id: 2,
+    title: "Fishing Competitions",
+    image: ArticleImg2,
+    icon: ArticleIcon2,
+    desc: "There are many variations of passages orem psum available but the majority have suffered alteration in some form by injected.",
+  },
+  {
+    id: 3,
+    title: "Fishing Equipments",
+    image: ArticleImg3,
+    icon: ArticleIcon3,
+    desc: "There are many variations of passages orem psum available but the majority have suffered alteration in some form by injected.",
+  },
+  {
+    id: 4,
+    title: "Fishing Tour",
+    image: ArticleImg4,
+    icon: ArticleIcon4,
+    desc: "There are many variations of passages orem psum available but the majority have suffered alteration in some form by injected.",
+  },
+];
+
+const ArticlesSection = () => {
+  return <div></div>;
+};
+
+export default ArticlesSection;
